5da2dd0b6e9a7054a322b6f21ef324b357b78a75
unknown <sumaiya.rrashid@gmail.comm>
2023-06-11 12:02:02 +0200

    delete added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vgqrech.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,6 +49,25 @@ async function run() {
             const result = await userCollection.insertOne(item)
             res.send(result)
         })
+        // get  selected Course data for email
+
+        app.get('/usersData', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
+            }
+            const result = await userCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.delete('/userData/:id', async(req, res)=>{
+            const  id = req.params.id
+            const query= {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
