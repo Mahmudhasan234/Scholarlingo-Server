936069195a0d590b0c968fac42e707dbaef0066c
unknown <sumaiya.rrashid@gmail.comm>
2023-06-14 02:31:24 +0200

    final commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const instructorCollection = client.db("scholarlingoDB").collection("instructors");
         const paymentCollection = client.db("scholarlingoDB").collection("payments");
